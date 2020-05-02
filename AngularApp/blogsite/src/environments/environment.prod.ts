a8b179675da26ff5c82050571653b3df92b7269f
shrawan <shrawankick@gmail.com>
2020-05-02 17:36:58 +0530

    added config

--- AngularApp/blogsite/src/environments/environment.prod.ts
+++ AngularApp/blogsite/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyADsER4_U9BL6GML4pfeMc8h2VxHRYcwYM",
+    authDomain: "blogsite-2cc58.firebaseapp.com",
+    databaseURL: "https://blogsite-2cc58.firebaseio.com",
+    projectId: "blogsite-2cc58",
+    storageBucket: "blogsite-2cc58.appspot.com",
+    messagingSenderId: "660961436525",
+    appId: "1:660961436525:web:8e6129c4d653be9cf43064",
+    measurementId: "G-ZN0RL5LRJ3"
+  } 
 };
